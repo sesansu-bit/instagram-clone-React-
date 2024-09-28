d3ee055505efb03835be73a4c2797053a08509fb
Seshansu sekhar swain <sesansuswain5@gmail.com>
2024-09-29 01:54:14 +0530

    Delete explorebox.jsx

--- explorebox.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./explorebox.module.css";
-const  Explorebox =({exploreimage} )=>{
-return(    
-<div className={styles["eachimg"]}><img src={exploreimage}/></div>
-)
-}
-export default Explorebox;
